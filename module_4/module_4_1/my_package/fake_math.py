6e78daca938b0853091728879f8259cdd993ac7c
alexe <alexeykozhakin@yandex.ru>
2024-08-09 19:08:57 +0200

    module4

---
+++ module_4/module_4_1/my_package/fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    return 'Ошибка' if second == 0 else first/second
+
+    # result = 'Ошибка' if second == 0 else first/second
+    # return result
+
+    # if second==0:
+    #     result = 'Ошибка'
+    # else:
+    #     result = first/second
+    # return result
---
+++ module_4/module_4_2.py
@@ -0,0 +1,13 @@
+
+
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+
+#======================= Начало программы ============================
+inner_function()
+test_function()
